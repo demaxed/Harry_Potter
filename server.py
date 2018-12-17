b4767ad86100bcf2f60346e070f8f18258125c30
Natalia Sokolovskaya <netka78087@gmail.com>
2018-12-17 22:49:33 +0300

    Added bootstrap and custom styles. Added navbar. Changed buttons from "search" to "login". Added login form. Added bottom padding from navbar.

--- server.py
+++ server.py
@@ -1,10 +1,15 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template, request
+my_flask_app = Flask(__name__)
 
-@app.route("/")
+@my_flask_app.route("/")
+def index():
+    return render_template('index.html')
 
-def hello():
-    return "Привет!"
-    
+@my_flask_app.route("/login/", methods=['POST'])
+def login():
+    return render_template('login.html', email=request.form.get('email'), password=request.form.get('pass'))
+
+
 if __name__=="__main__":
-    app.run()
+    my_flask_app.run(debug=True)
+    
