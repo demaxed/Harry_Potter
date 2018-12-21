e8a969e0a9b42472e437d7ba27d13a1ee8f1047d
Natalia Sokolovskaya <netka78087@gmail.com>
2018-12-22 01:00:27 +0300

    Added first quest step. Added buttons for choosing person.

--- server.py
+++ server.py
@@ -9,6 +9,13 @@ def index():
 def login():
     return render_template('login.html', email=request.form.get('email'), password=request.form.get('pass'))
 
+@my_flask_app.route("/dialog_harry", methods=['POST'])
+def dialog_harry():
+    return render_template('dialog1.html')
+
+@my_flask_app.route('/dialog1/')
+def dialog1():
+    return render_template('dialog1.html')
 
 if __name__=="__main__":
     my_flask_app.run(debug=True)
