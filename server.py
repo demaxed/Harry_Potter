a62d9b67538bbed9421d3c174c321441d145779e
Natalia Sokolovskaya <netka78087@gmail.com>
2019-01-09 22:44:41 +0300

    Dialog modified to universal dialog: Added dialog_character route, view function, template. Changed dialog template extension to jinja2.

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request
+from flask import (
+    Flask, Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
 my_flask_app = Flask(__name__)
 
 @my_flask_app.route("/")
@@ -9,13 +11,28 @@ def index():
 def login():
     return render_template('login.html', email=request.form.get('email'), password=request.form.get('pass'))
 
-@my_flask_app.route("/dialog_harry", methods=['POST'])
-def dialog_harry():
-    return render_template('dialog1.html')
+@my_flask_app.route("/dialog_character", methods=['POST'])
+def dialog_character():
+    dialog = {
+        'text': "olololo ggg",
+        'jumps': [
+            {
+                'dialog_id': 50,
+                'text': "Нажми сюда, чтобы ололо"
+            },
+            {
+                'dialog_id': 49,
+                'text': "Нажми сюда, чтобы гггг"
+            }
+        ]
+    }
 
-@my_flask_app.route('/dialog1/')
-def dialog1():
-    return render_template('dialog1.html')
+    # return redirect(url_for("dialog1"))
+    return render_template('dialog.jinja2', dialog=dialog)
+
+# @my_flask_app.route('/dialog1/')
+# def dialog1():
+#     return render_template('dialog1.html')
 
 if __name__=="__main__":
     my_flask_app.run(debug=True)
